33be865c6d4749b5764ed8fc4c87237b2b76c7d7
Emanuel Xavier <emanueljx@outlook.com>
2023-08-28 17:33:19 -0300

    keymaps for tabs added

--- nvim/.config/lua/emanuel-xavier/remap.lua
+++ nvim/.config/lua/emanuel-xavier/remap.lua
@@ -1,2 +1,10 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex) -- List directory 
+
+
+vim.keymap.set("n", "<leader>nt", ":exec 'tabnew'<CR>") -- create a new tab
+vim.keymap.set("n", "<leader>ct", ":exec 'tabclose'<CR>") -- close current tab
+vim.keymap.set("n", "<Tab>", ":tabnext<CR>", { noremap = true, silent = true }) -- go to the next tab
+vim.keymap.set("n", "<S-Tab>", ":tabprevious<CR>", { noremap = true, silent = true }) -- go the the previous tab
+
+vim.keymap.set("n", "<leader>pf", ":exec 'e#'<CR>") -- go back to the previous file
