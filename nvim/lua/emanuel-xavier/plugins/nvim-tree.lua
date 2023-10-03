b8c3a5c184dc96d7bb4eef3d4487322fb187e673
emanuel-xavier <emanueljx@outlook.com>
2023-10-03 18:49:24 -0300

    nvim tree toggle command

--- nvim/lua/emanuel-xavier/plugins/nvim-tree.lua
+++ nvim/lua/emanuel-xavier/plugins/nvim-tree.lua
@@ -1,6 +1,8 @@
 return {
   "nvim-tree/nvim-tree.lua",
   config = function()
-    
+    require("nvim-tree").setup({
+      vim.api.nvim_set_keymap('n', '<leader>ft', ':NvimTreeToggle<CR>', { noremap = true, silent = true }) 
+    }) 
   end
 }
