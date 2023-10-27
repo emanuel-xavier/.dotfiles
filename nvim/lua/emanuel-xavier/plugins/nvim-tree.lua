43ff4a367ce3dac088ec38b4efc4ba2eb3d2c1a4
emanuel-xavier <emanueljx@outlook.com>
2023-10-27 09:06:04 -0300

    keymap to focus on the nvim tree

--- nvim/lua/emanuel-xavier/plugins/nvim-tree.lua
+++ nvim/lua/emanuel-xavier/plugins/nvim-tree.lua
@@ -2,7 +2,8 @@ return {
   "nvim-tree/nvim-tree.lua",
   config = function()
     require("nvim-tree").setup({
-      vim.api.nvim_set_keymap('n', '<leader>ft', ':NvimTreeToggle<CR>', { noremap = true, silent = true }) 
+      vim.api.nvim_set_keymap('n', '<leader>e', ':NvimTreeFocus<CR>', { noremap = true, silent = true }),
+      vim.api.nvim_set_keymap('n', '<leader>ft', ':NvimTreeToggle<CR>', { noremap = true, silent = true }),
     }) 
   end
 }
