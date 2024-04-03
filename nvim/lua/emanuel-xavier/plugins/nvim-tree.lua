af76cdf94f408d65542ba0914ca00a248b691ede
emanuel-xavier <emanueljx@outlook.com>
2024-04-02 21:42:41 -0300

    set nvimtree position o the right of the screen

--- nvim/lua/emanuel-xavier/plugins/nvim-tree.lua
+++ nvim/lua/emanuel-xavier/plugins/nvim-tree.lua
@@ -4,6 +4,9 @@ return {
     require("nvim-tree").setup({
       vim.api.nvim_set_keymap('n', '<leader>e', ':NvimTreeFocus<CR>', { noremap = true, silent = true }),
       vim.api.nvim_set_keymap('n', '<leader>ft', ':NvimTreeToggle<CR>', { noremap = true, silent = true }),
+      view = {
+        side = "right",
+      },
     }) 
   end
 }
