d35c0969aac1e92ba2b2ea9a683d2e95087029b8
emanuel-xavier <emanueljx@outlook.com>
2024-05-02 23:02:23 -0300

    adds function to install zsh and dependencies on arch or ubuntu

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
 
-apt install -y curl gcc make default-libmysqlclient-dev libssl-dev \
-  zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev llvm \
-  libncurses5-dev libncursesw5-dev \
-  xz-utils tk-dev libffi-dev liblzma-dev python3-openssl git \
-  zsh
+install_packages() {
+    if [ -f /etc/os-release ] && grep -qi ubuntu /etc/os-release; then
+        sudo apt install -y curl gcc make default-libmysqlclient-dev libssl-dev \
+            zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev llvm \
+            libncurses5-dev libncursesw5-dev \
+            xz-utils tk-dev libffi-dev liblzma-dev python3-openssl git \
+            zsh
+    elif [ -f /etc/arch-release ]; then
+        sudo pacman -S --needed \
+            curl gcc make mariadb-libs openssl zlib \
+            libutil-linux bzip2 sqlite llvm ncurses \
+            xz tk libffi xz python-openssl git zsh
+    else
+        echo "Unsupported distribution. Please install the required packages manually."
+        exit 1
+    fi
 
+    echo "Required packages installed successfully."
+}
+
+install_packages
 chsh -s /bin/zsh
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
