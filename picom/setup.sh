efcfe2e3622bb39df239889543e262e065f55b44
emanuel-xavier <emanueljx@outlook.com>
2024-05-02 22:59:48 -0300

    adds function to install picom on arch or ubuntu

--- picom/setup.sh
+++ picom/setup.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
-apt install picom -y
+install_picom() {
+    if [ -f /etc/arch-release ]; then
+        sudo pacman -S picom --noconfirm
+    elif [ -f /etc/os-release ] && grep -qi ubuntu /etc/os-release; then
+        sudo apt install picom -y
+    else
+        echo "Unsupported distribution. Please install picom manually."
+        exit 1
+    fi
+}
 
-rm -rf ~/.config/picom.conf 
-ln -s $(pwd)/picom.conf ~/.config/picom.conf
+install_picom
+
+rm -rf ~/.config/picom.conf
+ln -s "$(pwd)/picom.conf" ~/.config/picom.conf
+echo "Picom installed and configured successfully."
