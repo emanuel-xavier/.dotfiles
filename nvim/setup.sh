535981acff44d7f751f88c08cd50e455005622a4
emanuel-xavier <emanueljx@outlook.com>
2024-05-02 23:04:24 -0300

    adds function to install neovim on arch

--- nvim/setup.sh
+++ nvim/setup.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
 
+install_neovim() {
+  if [ -f /etc/arch-release ]; then
+    sudo pacman -S --needed neovim 
+  fi
+}
+
+install_neovim
+
 rm -rf /home/$USER/.config/nvim
 ln -s $(pwd) /home/$USER/.config/nvim
