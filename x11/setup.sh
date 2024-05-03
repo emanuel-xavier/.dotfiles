24db85225acb670d748e6b135e87a533cb2c0e54
emanuel-xavier <emanueljx@outlook.com>
2024-05-02 23:07:58 -0300

    adds function to install xorg input lib for arch and ubuntu

--- x11/setup.sh
+++ x11/setup.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
-apt install xserver-xorg-input-libinput
+install_xorg_input_lib() {
+  if [ -f /etc/os-release ] && grep -qi ubuntu /etc/os-release; then
+    sudo apt install -y xserver-xorg-input-libinput
 
+    elif [ -f /etc/arch-release ]; then
+        sudo pacman -S --needed xf86-input-libinput
+          
+    else
+        echo "Unsupported distribution. Please install the required packages manually."
+        exit 1
+    fi
+}
+
+install_xorg_input_lib
 ln -s ./50-synaptics.conf /etc/x11/xorg.conf.d/50-synaptics.conf
