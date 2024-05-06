13db66ccbb0f5fd26478fe589914f33389814087
emanuel-xavier <emanueljx@outlook.com>
2024-05-06 16:46:08 -0300

    creates screenshots folder for flameshot

--- i3/setup.sh
+++ i3/setup.sh
@@ -2,6 +2,8 @@
 
 sudo apt install i3 i3blocks flameshot nitrogen -y
 
+mkdir -p ~/Pictures/screenshots
+
 rm -rf ~/.config/i3
 ln -s $(pwd) ~/.config/i3
 ln -s $(pwd)/i3blocks.conf ~/.i3blocks.conf
