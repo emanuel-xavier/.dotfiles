76bc9c1cc4db75e146865af98d8d56b48fb07adf
emanuel-xavier <emanueljx@outlook.com>
2024-05-23 20:54:38 -0300

    update custom paths

--- .paths.sh
+++ .paths.sh
@@ -4,10 +4,11 @@ add_folder_to_path() {
   export PATH="$PATH:$1"
 }
 
-PATHS_TO_ADD=( "/home/emanuel/.go/bin"    \
-               "/home/emanuel/.nvim/bin"  \
-               "/home/emanuel/.cargo/bin" \
-               "/home/emanuel/.node/bin"  )
+PATHS_TO_ADD=( )
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 for i in "${PATHS_TO_ADD[@]}"; do
   add_folder_to_path "$i"
