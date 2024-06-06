7c36de5fc01c3547256036012f2624f74a5e10fc
emanuel-xavier <emanueljx@outlook.com>
2024-06-05 21:01:05 -0300

    remove '+' and 'ºC' from temperature

--- i3/polybar/cpu_temperature.sh
+++ i3/polybar/cpu_temperature.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cpu_temp=$(sensors | grep 'k10temp-pci-00c3' -A 3 | grep 'Tctl' | awk '{print $2}')
+cpu_temp=$(sensors | grep 'k10temp-pci-00c3' -A 3 | grep 'Tctl' | awk '{print $2}' | tr -d '+°C')
 
 # Output the temperatures
 echo "$cpu_temp"
--- i3/polybar/gpu_temperature.sh
+++ i3/polybar/gpu_temperature.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-gpu_temp=$(sensors | grep 'amdgpu-pci-0300' -A 10 | grep 'edge' | awk '{print $2}')
+gpu_temp=$(sensors | grep 'amdgpu-pci-0300' -A 10 | grep 'edge' | awk '{print $2}' | tr -d '+°C')
 
 # Output the temperatures
 echo "$gpu_temp"
