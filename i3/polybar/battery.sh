89681ac42370c2a29d99c01e7d922984fb53dc3e
emanuel-xavier <emanueljx@outlook.com>
2024-06-18 11:35:06 -0300

    add a module that shows the battery status

---
+++ i3/polybar/battery.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+icon_color=$1
+text_color=$2
+
+# https://github.com/emmyvinte/polybar-battery.sh
+
+# Coloque aqui o local que identifica sem seu dispositivo está carregando ou descarregando
+# IMPORTANTE ! A saída deve ser em binário, caso contrário resultará em erro!
+is_plugged=$(cat /sys/class/power_supply/AC0/online)
+
+# Coloque aqui o local que identifica o número relativo a porcentagem da bateria de seu dispositivo
+battery_level=$(cat /sys/class/power_supply/BAT0/capacity)
+
+# EXTRA! Altere os ícones para qualquer um de sua preferência, assim como os valores da %
+# EXTRA! Customize a cor do ícone começando com %{F#} e fechando com %{F-}
+if [ "$is_plugged" -eq 1 ]; then
+    icon="%{F$icon_color} %{F-}"
+else
+    if [ "$battery_level" -le 15 ]; then
+        icon="%{F#$icon_color} %{F-}"
+    elif [ "$battery_level" -le 25 ]; then
+        icon="%{F#$icon_color} %{F-}"
+    elif [ "$battery_level" -le 60 ]; then
+        icon="%{F#$icon_color} %{F-}"
+    elif [ "$battery_level" -le 90 ]; then
+        icon="%{F#$icon_color} %{F-}"
+    else
+        icon="%{F#$icon_color} %{F-}"
+    fi
+fi
+
+echo "$icon ${F#$text_color}$battery_level%"
