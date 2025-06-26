541f9545f2de6fd1a24e36b66deed19e095c7ad9
emanuel-xavier <emanueljx@outlook.com>
2025-06-26 16:46:56 -0300

    changes the font

--- i3/setup.sh
+++ i3/setup.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
-sudo pacman -Sy i3 flameshot nitrogen
+sudo pacman -Sy i3 flameshot nitrogen libnotify ttf-martian-mono-nerd
 
 mkdir -p ~/Pictures/screenshots
+
+fc-cache -fv
 
 rm -rf ~/.config/i3
 ln -s $(pwd) ~/.config/i3
--- kitty/setup.sh
+++ kitty/setup.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-sudo pacman -Sy kitty
+sudo pacman -Sy kitty ttf-martian-mono-nerd
+
+fc-cache -fv
 
 rm -rf ~/.config/kitty
 ln -s $(pwd) ~/.config/kitty
