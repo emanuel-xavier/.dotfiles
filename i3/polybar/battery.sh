221f6f1493f253e7d9229826e02c561642275aff
emanuel-xavier <emanueljx@outlook.com>
2025-09-23 17:25:05 -0300

    feat: send notification about low battery

--- i3/polybar/battery.sh
+++ i3/polybar/battery.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 icon_color=$1
 text_color=$2
@@ -12,6 +12,30 @@ is_plugged=$(cat /sys/class/power_supply/AC0/online)
 # Coloque aqui o local que identifica o número relativo a porcentagem da bateria de seu dispositivo
 battery_level=$(cat /sys/class/power_supply/BAT0/capacity)
 
+# File to store notification state
+notification_state_file="/tmp/battery_notification_state"
+
+send_notification() {
+    urgency=$1
+    current_time=$(date +%s)
+    last_notification_time=0
+    last_notification_level=0
+
+    if [ -f "$notification_state_file" ]; then
+        last_notification_time=$(cat "$notification_state_file" | cut -d' ' -f1)
+        last_notification_level=$(cat "$notification_state_file" | cut -d' ' -f2)
+    fi
+
+    time_diff=$((current_time - last_notification_time))
+    level_diff=$((last_notification_level - battery_level))
+
+    if [ "$time_diff" -gt 120 ] || [ "$level_diff" -ge 5 ]; then
+        notify-send "batery level: $battery_level" -u "$urgency" -i ~/.config/i3/polybar/low-batery.jpg
+        echo "$current_time $battery_level" > "$notification_state_file"
+    fi
+}
+
+
 # EXTRA! Altere os ícones para qualquer um de sua preferência, assim como os valores da %
 # EXTRA! Customize a cor do ícone começando com %{F#} e fechando com %{F-}
 if [ "$is_plugged" -eq 1 ]; then
@@ -19,10 +43,10 @@ if [ "$is_plugged" -eq 1 ]; then
 else
     if [ "$battery_level" -le 15 ]; then
         icon="%{F#$icon_color} %{F-}"
-        notify-send "batery level: $battery_level" -u critical -i ~/.config/i3/polybar/low-batery.jpg
+        send_notification "critical"
     elif [ "$battery_level" -le 25 ]; then
         icon="%{F#$icon_color} %{F-}"
-        notify-send "batery level: $battery_level" -u low -i ~/.config/i3/polybar/low-batery.jpg
+        send_notification "low"
     elif [ "$battery_level" -le 60 ]; then
         icon="%{F#$icon_color} %{F-}"
     elif [ "$battery_level" -le 90 ]; then
