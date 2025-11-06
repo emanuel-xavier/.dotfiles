d7b25128e582df7d110c45f3f8882de2a1f8ce84
emanuel-xavier <emanueljx@outlook.com>
2025-11-05 22:56:57 -0300

    fix whitchkey warnings

--- nvim/lua/plugins/whichkey.lua
+++ nvim/lua/plugins/whichkey.lua
@@ -1,17 +1,18 @@
 return {
   'folke/which-key.nvim',
   event = 'VimEnter',
+  dependencies = {
+    'echasnovski/mini.icons',
+  },
   config = function()
     -- gain access to the which key plugin
     local which_key = require('which-key')
 
     -- call the setup function with default properties
-    which_key.setup()
-
-    -- Register prefixes for the different key mappings we have setup previously
-    which_key.register({
-        -- ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
-        ['<leader>J'] = { name = '[J]ava', _ = 'which_key_ignore' },
+    which_key.setup({
+      spec = {
+        { "<leader>J", group = "Java" },
+      }
     })
   end
 }
